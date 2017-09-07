062b610e37af6e21ded591b9c691675c0fe4041c
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-09-07 14:15:25 +0200

    Basic setup
    Setup dev environment + app/ folder structure

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('sass', function () {
+  gulp.src('./app/src/sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded'} ).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe( autoprefixer({ browsers: ['last 2 versions'] }) )
+    .pipe(gulp.dest('./app/assets/css/'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('serve', ['sass'], function () {
+  browserSync.init({
+    server: "./app",
+    injectChanges: true,
+    notify: false
+  })
+
+  gulp.watch('./app/src/sass/*.scss', ['sass'])
+  gulp.watch('./app/*.html').on('change', browserSync.reload)
+})
+
+gulp.task('default', ['serve'])
