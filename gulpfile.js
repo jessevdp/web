1645d2b28ab86f5adef5e830930118d67755bbaf
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2017-09-28 13:42:51 +0200

    Create JS task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('sass', function () {
-  gulp.src('./app/src/sass/**/*.scss')
+  return gulp.src('./app/src/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'expanded'} ).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe( autoprefixer({ browsers: ['last 2 versions'] }) )
     .pipe(gulp.dest('./app/assets/css/'))
     .pipe(browserSync.stream())
+})
+
+gulp.task('js', function () {
+  return gulp.src('./app/src/js/**/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./app/assets/js/'))
 })
 
 gulp.task('serve', ['sass'], function () {
